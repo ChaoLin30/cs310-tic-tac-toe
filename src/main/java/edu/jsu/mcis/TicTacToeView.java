65844074b885cb1c171eb3a3f7bd1392bbe9ad4c
Chao Lin <Clin1@stu.jsu.edu>
2018-09-24 16:56:11 -0500

    view

--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -1,83 +1,74 @@
 package edu.jsu.mcis;
 
-import edu.jsu.mcis.TicTacToeModel.Mark;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class TicTacToeView {
-
-    private TicTacToeModel model;
+public class TicTacToeView extends JPanel implements ActionListener {
     
-    /* CONSTRUCTOR */
-	
-    public TicTacToeView(TicTacToeModel model) {
-        
-        this.model = model;
-        
-    }
-	
-    public void viewModel() {
-        
-        /* Print the board to the console (see examples) */
-        
+    TicTacToeModel model;
 
-        System.out.print("\n  ");
-		for (int i = 0; i < model.getWidth(); i++) {
-			System.out.print(i);
-		}
-		System.out.print("\n\n");
-		for (int i = 0; i < model.getWidth(); i++) {
-			System.out.print(i + " ");
-			for (int j = 0; j < model.getWidth(); j++) {
-				if(model.getMark(i,j) == TicTacToeModel.Mark.EMPTY) {
-					System.out.print("-");
-                }
+    private JButton[][] squares;
+    private JPanel squaresPanel;
+    private JLabel resultLabel;
+
+    public TicTacToeView(TicTacToeModel model) {
+
+        this.model = model;
+
+        int width = model.getWidth();
+
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        squares = new JButton[width][width];
+        squaresPanel = new JPanel(new GridLayout(width,width));
+        resultLabel = new JLabel();
+        resultLabel.setName("ResultLabel");
+        
+        for (int row = 0; row < width; row++){
+            
+            for (int col = 0; col < width; col++){
                 
-				else {
-					System.out.print(model.getMark(i,j));
-			
-				}
-			}
-            System.out.print("\n");
+                squares[row][col] = new JButton(); 
+                squares[row][col].addActionListener(this);
+                squares[row][col].setName("Square" + row + col);
+                squares[row][col].setPreferredSize(new Dimension(64,64));
+                squaresPanel.add(squares[row][col]);
+                
+            }
             
         }
+
+        this.add(squaresPanel);
+        this.add(resultLabel);
         
-		System.out.print("\n\n\n");
-		
-        
-        
-    }
-
-
-    
-
-    public void showNextMovePrompt() {
-
-        /* Display a prompt for the player's next move (see examples) */
-
-        if(model.isXTurn()){
-                System.out.println("Player 1 (X) Move: ");
-        }
-        else{
-                System.out.println("Player 2 (O) Move:");
-        }
-        
-        System.out.println("Enter the row and column numbers, separated by a space:");
+        resultLabel.setText("Welcome to Tic-Tac-Toe!");
 
     }
 
-    public void showInputError() {
-
-        /* Display an error if input is invalid (see examples) */
-
-        System.out.println("input is invalid");
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        
+        /* Handle button clicks.  Extract the row and col values from the name
+           of the button that was clicked, then make the mark in the grid using
+           the Model's "makeMark()" method.  Finally, use the "updateSquares()"
+           method to refresh the View.  If the game is over, show the result
+           (from the Model's "getResult()" method) in the result label. */
+        
+        String name = ((JButton) event.getSource()).getName(); // Get button name
+        
+        // INSERT YOUR CODE HERE
 
     }
+        
+    public void updateSquares() {
 
-    public void showResult(String r) {
-
-        /* Display final winner */
-
-        System.out.println(r + "!");
+        /* Loop through all View buttons and (re)set the text of each button
+           to reflect the grid contents (use the Model's "getMark()" method). */
 
     }
-	
+        
+    public void showResult(String message) {
+        resultLabel.setText(message);
+    }
+
 }
